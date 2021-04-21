2dc2030f554c7a6660f32fa37b31847d2276fdb7
成丰快运 <>
2021-04-21 12:00:37 +0800

    提交代码

---
+++ Example/SuperPopup/APopupView.swift
@@ -0,0 +1,72 @@
+//
+//  APopupView.swift
+//  SuperPopup_Example
+//
+//  Created by 成丰快运 on 2021/4/21.
+//  Copyright © 2021 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class APopupView: UIView {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    init(_ imgName: String) {
+        
+        let size = UIScreen.main.bounds.size
+        var frame = CGRect.zero
+        let ratio = 0.5
+        
+        if imgName == "popup-1" {
+            frame = CGRect.init(x: 0, y: 0, width: 901 * ratio, height: 927 * ratio)
+        }else if imgName == "popup-2" {
+            frame = CGRect.init(x: 0, y: 0, width: 1119 * ratio, height: 670 * ratio)
+        }else if imgName == "popup-3" {
+            frame = CGRect.init(x: 0, y: 0, width: 1196 * ratio, height: 234 * ratio)
+        }else if imgName == "popup-4" {
+            frame = CGRect.init(x: 0, y: 0, width: 1062 * ratio, height: 1423 * ratio)
+        }else if imgName == "popup-5" {
+            frame = CGRect.init(x: 0, y: 0, width: 1242 * ratio, height: 948 * ratio)
+        }else if imgName == "popup-6" {
+            frame = CGRect.init(x: 0, y: 0, width: 493 * ratio, height: 485 * ratio)
+        }else if imgName == "popup-7" {
+            frame = CGRect.init(x: 0.0, y: 0.0, width: 1123.0 / size.height / 2208.0, height: 2208.0)
+        }else if imgName == "popup-8" {
+            frame = CGRect.init(x: 0.0, y: 0.0, width: 974.0 / size.height / 2208.0, height: 2208.0)
+        }else if imgName == "popup-9" {
+            frame = CGRect.init(x: 0, y: 0, width: size.width, height: size.height)
+        }else if imgName == "popup-10" {
+            frame = CGRect.init(x: 0, y: 0, width: size.width, height: size.height)
+        }else if imgName == "popup-11" {
+            frame = CGRect.init(x: 0, y: 0, width: 658 * ratio, height: 969 * ratio)
+        }else if imgName == "popup-bg-1" {
+            frame = CGRect.init(x: 0, y: 0, width: size.width, height: size.height)
+        }else if imgName == "popup-bg-2" {
+            frame = CGRect.init(x: 0, y: 0, width: 670.0 / (502.0 / size.width), height: size.width)
+        }else if imgName == "popup-bg-3" {
+            frame = CGRect.init(x: 0, y: 0, width: size.width, height: size.height)
+        }else if imgName == "share_friend" {
+            frame = CGRect.init(x: 0, y: 0, width: 120 * ratio, height: 120 * ratio)
+        }else if imgName == "share_link" {
+            frame = CGRect.init(x: 0, y: 0, width: 120 * ratio, height: 120 * ratio)
+        }else if imgName == "share_message" {
+            frame = CGRect.init(x: 0, y: 0, width: 120 * ratio, height: 120 * ratio)
+        }else if imgName == "share_qq_zone" {
+            frame = CGRect.init(x: 0, y: 0, width: 120 * ratio, height: 120 * ratio)
+        }else if imgName == "share_qq" {
+            frame = CGRect.init(x: 0, y: 0, width: 120 * ratio, height: 120 * ratio)
+        }else if imgName == "share_weibo" {
+            frame = CGRect.init(x: 0, y: 0, width: 120 * ratio, height: 120 * ratio)
+        }else if imgName == "share_weixin" {
+            frame = CGRect.init(x: 0, y: 0, width: 120 * ratio, height: 120 * ratio)
+        }
+        super.init(frame: frame)
+        self.imageView.image = UIImage.init(named: imgName)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
--- Example/SuperPopup/ViewController.swift
+++ Example/SuperPopup/ViewController.swift
@@ -8,18 +8,77 @@
 
 import UIKit
 import SuperPopup
-class ViewController: UIViewController {
 
+class TestModel: NSObject {
+    
+    var sectionName:String = ""
+    
+    var data:[String] = []
+
+    init(_ name:String,_ data:[String]) {
+        self.sectionName = name
+        self.data = data
+    }
+}
+
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    
+   
+
+    @IBOutlet weak var tableView: UITableView!
+    
     var showed:Bool = false
     
     let alert = Bundle.main.loadNibNamed("AlertView", owner: nil, options: nil)?.first as? AlertView
 
+    var dataSource:[TestModel] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-      
+
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "UITableViewCell")
+        
+        let base = TestModel.init("基础动画", ["无动画","渐隐","滑动","缩放","折叠","泡泡","旋转:XYZ","遮罩"])
+        let group = TestModel.init("多种自由组合动画", ["渐隐+滑动","滑动+缩放","渐隐+滑动+缩放","滑动+折叠","渐隐泡泡","旋转Z","旋转Z+Y","旋转Z+Y+X","旋转+遮罩","三角形遮罩","圆形遮罩"])
+        
+        self.dataSource.append(base)
+        self.dataSource.append(group)
 
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.tableView.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 36;
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let model:TestModel = self.dataSource[section]
+        return model.sectionName
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return self.dataSource.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let model:TestModel = self.dataSource[section]
+        return model.data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let model:TestModel = self.dataSource[indexPath.section]
+        let name = model.data[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell")
+        cell?.textLabel?.text = name
+        return cell!
+    }
+    
     
     //1.滑动时offset有问题
     //2.考虑隐藏时动画不同的问题
